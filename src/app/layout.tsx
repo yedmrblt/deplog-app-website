6e342851abaef950a6ccf082b3210b985d7a1ca8
Yunus Emre Demirbulut <y.emre.demirbulut@gmail.com>
2025-02-10 09:16:28 +0300

    bump version to 1.0.0 and update layout styles for better centering

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -41,7 +41,7 @@ export default function RootLayout({
       lang="tr"
       className={cx("scroll-smooth", fontText.variable, fontDisplay.variable)}
     >
-      <body className="text-default flex min-h-svh bg-[#fefefe] text-sm text-stone-800 antialiased sm:text-base">
+      <body className="text-default flex min-h-svh justify-center bg-[#fefefe] text-sm text-stone-800 antialiased sm:text-base">
         <main className="sm:pt-42 xl:max-w-screen-lg xl:max-w-screen-lg flex flex-1 justify-center px-8 pt-24 pb-16 sm:pt-32 sm:pb-28 xl:px-36">
           {children}
         </main>
